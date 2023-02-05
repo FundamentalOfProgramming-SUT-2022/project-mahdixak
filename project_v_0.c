0ecc2352ce8a5de8ba4ba7b5123ea103baef45fe
mahdixak <115116914+mahdixak@users.noreply.github.com>
2023-02-05 04:22:52 +0330

    last commit

--- project_v_0.c
+++ project_v_0.c
@@ -28,6 +28,13 @@ void cutstr(const char *address,char *position,int size,char *attribute) ;
 void treeshow(int depth) ;
 void listfiles(const char* dirname) ;
 void grep(const char *text,char *addresses,char *attribute);
+void pastestr(const char *address,const char *position);
+void comprator(char *path1,char *path2) ;
+
+
+void search(const char *address,const char *text,char *attribute);
+void closingpair(char *address) ;
+
 
 
 
@@ -152,13 +159,11 @@ void command_search(char input[])
                     }
                     if(index==5) {
                         //position
-                        char l;
+                        char *l;
                         l = strtok(diraddress,":");
-                        l = atoi(l);
-                        line = l;
+                        line = atoi(l);
                         l = strtok(NULL,":");
-                        l = atoi(l);
-                        pos = l;
+                        pos = atoi(l);
                     }
                     diraddress = strtok(NULL," ");
                     index ++;
@@ -189,7 +194,7 @@ void command_search(char input[])
         else if (strstr(input, "removestr --file"))
         {
             strremove(input,"removestr --file ");
-            printf("%s\n" ,input);
+            //printf("%s\n" ,input);
             char path[MAX];
             char position[20];
             char attribute[20];    
@@ -479,6 +484,7 @@ void command_search(char input[])
             printf("Oops! you enter INVALID command!\n");
         }
     }
+    //compare
     else if (strstr(input,"compare"))
     {
         strremove(input,"compare ");
@@ -641,13 +647,11 @@ void read_file(const char *address)
 
 void removestr(const char *address,char *position,int size,char*attribute)
 {
-    int l;
+    char *l;
     l = strtok(position,":");
-    l = atoi(l);
-    int line = l;
+    int line = atoi(l);
     l = strtok(NULL,":");
-    l = atoi(l);
-    int pos = l;
+    int pos = atoi(l);
     ////////////////////////////
     FILE *file;
     FILE *temp;
@@ -681,7 +685,26 @@ void removestr(const char *address,char *position,int size,char*attribute)
     }
     while(keep_reading == true){
         fgets(buffer , MAX ,file) ;
-        if (feof(file)) keep_reading = false ;
+        if (feof(file)) {
+            keep_reading = false ;
+            if (buffer[0]==' ') fputs(buffer,temp);
+            if(current_l == line){
+                memcpy(newline  , buffer , pos) ;   
+                int temp_size = strlen(newline) ;  
+                for(int i  = 0 ; i < size ; i++){
+                    newline[temp_size + i ] = NULL ; 
+                    //for clip board
+                }
+                //printf("buffer is :%s\n" ,buffer);
+                int x = pos;
+                for (int j=2*pos-1;j<strlen(buffer);j++) {
+                    newline[x] = buffer[j];
+                    x ++;
+                }
+                fputs(newline , temp) ; 
+                //fputc('\n' ,temp); 
+            }           
+        }
         else {
             if(current_l == line){
                 memcpy(newline  , buffer , pos) ;   
@@ -716,13 +739,11 @@ void copystr(const char*address,const char *position,int size,char *attribute)
     for (int i=0;i<strlen(clipboard);i++) {
         clipboard[i] = '\0';
     }    
-    int l;
+    char *l;
     l = strtok(position,":");
-    l = atoi(l);
-    int line = l;
+    int line = atoi(l);
     l = strtok(NULL,":");
-    l = atoi(l);
-    int pos = l;
+    int pos = atoi(l);
     ////////////////////////////
     strremove(address,"/root/");
     FILE *file;
@@ -768,13 +789,11 @@ void cutstr(const char *address,char *position,int size,char *attribute) {
     for (int i=0;i<strlen(clipboard);i++) {
         clipboard[i] = '\0';
     }
-    int l;
+    char *l;
     l = strtok(position,":");
-    l = atoi(l);
-    int line = l;
+    int line = atoi(l);
     l = strtok(NULL,":");
-    l = atoi(l);
-    int pos = l;
+    int pos = atoi(l);
     ////////////////////////////
     FILE *file;
     FILE *temp;
@@ -808,7 +827,27 @@ void cutstr(const char *address,char *position,int size,char *attribute) {
     }
     while(keep_reading == true){
         fgets(buffer , MAX ,file) ;
-        if (feof(file)) keep_reading = false ;
+        if (feof(file)) {
+            keep_reading = false ;
+            if (buffer[0]==' ') fputs(buffer,temp);
+            if(current_l == line){
+                memcpy(newline  , buffer , pos) ;   
+                int temp_size = strlen(newline) ;  
+                for(int i  = 0 ; i < size ; i++){
+                    newline[temp_size + i] = NULL ; 
+                    //for clip board
+                    clipboard[i] = buffer[temp_size+i];
+                }
+                //printf("buffer is :%s\n" ,buffer);
+                int x = pos;
+                for (int j=2*pos-1;j<strlen(buffer);j++) {
+                    newline[x] = buffer[j];
+                    x ++;
+                }
+                fputs(newline , temp) ; 
+                //fputc('\n' ,temp);
+            }
+        }
         else {
             if(current_l == line){
                 memcpy(newline  , buffer , pos) ;   
@@ -837,18 +876,16 @@ void cutstr(const char *address,char *position,int size,char *attribute) {
     fclose(temp);
     remove(temp_a);
     rename(secondpath,temp_a);
-    printf("clip board is :%s\n" ,clipboard);
+    //printf("clip board is :%s\n" ,clipboard);
 }
 
 void pastestr(const char *address,const char *position)
 {
-    int l;
+    char *l;
     l = strtok(position,":");
-    l = atoi(l);
-    int line = l;
+    int line = atoi(l);
     l = strtok(NULL,":");
-    l = atoi(l);
-    int pos = l;
+    int pos = atoi(l);
     ////////////////////////////
     FILE *file;
     FILE *temp;
@@ -904,17 +941,27 @@ void pastestr(const char *address,const char *position)
     rename(secondpath,temp_a);
 }
 
-void search(const char *find,const char *text,char *attribute)
+void search(const char *address,const char *text,char *attribute)
 {
+    if (strstr(address,"/root/")) strremove(address,"/root/");
     if (strstr(attribute,"-at")&&strstr(attribute,"all")) {
         printf("It can't be possible dude!\n");
         return ;
     }
+    FILE *file = fopen(address , "r") ;
+    if (file==NULL) {
+        printf("Can't read the file\n");
+        return ;
+    }
+    char buffer[MAX];
     bool count = false;
+    int count_num = 0;
     bool at = false ;
     int num=0;
     bool byword = false ;
     bool all = false ;
+    int all_num[MAX];
+    int an = 0;
     bool wildcard = false;
     char *temp = (char *)malloc(sizeof(char)*MAX);
     temp = strtok(attribute," ");
@@ -936,16 +983,126 @@ void search(const char *find,const char *text,char *attribute)
             all = true;
 
         }
-        printf("%s\n" ,temp);
         temp = strtok(NULL," ");
     }
-    if (byword) {
+    // wildcard search
+    int wildcard_s = 0;
+    if (strstr(text,"*")) wildcard = true;
+    //find
+    bool keep_reading = true;
+    int line = 0;
+    if (wildcard) {
+        //wildcard mode //INCOMPLITE
+        if (text[0]=='*') {
+            wildcard_s = 1; 
+        }
+        else if (text[strlen(text)-1]) {
+            wildcard_s = -1;
+        }
+        printf("wildcard_s is :%d\n" ,wildcard_s);
+        while (keep_reading) {
+            fgets(buffer,MAX,file);
+            line ++ ;
+            bool vaild = false;
+            for (int i=0;i<strlen(buffer);i++) {
+                if (wildcard_s==-1) {
+                    if (text[0]==buffer[i]) {
+                        vaild = true;
+                        while (buffer[i]!=' ')
+                        {
+                            i++;
+                        }
+                        int tmp = strlen(buffer);
+                        if (vaild==true) {
+                            all_num[an] = ftell(file)-line-tmp;
+                            an ++;
+                            count_num ++;
+                        }  
+                    }
+                }
+                else if (wildcard_s==1) {
 
+                }
+            }
+            if (feof(file)) keep_reading = false;
+        }    
     }
     else {
-
+        //normal mode
+        while (keep_reading) {
+            fgets(buffer,MAX,file);
+            line ++ ;
+            bool vaild = false;
+            for (int i=0;i<strlen(buffer);i++) {
+                if (text[0]==buffer[i]) {
+                    vaild = true;
+                    for (int a=1;a<strlen(text);a++) 
+                    {
+                        if (text[a]==buffer[a+i]) {
+                            vaild = true ;
+                        }
+                        else {
+                            vaild = false;
+                        }
+                    }
+                    int tmp = strlen(buffer);
+                    if (vaild==true) {
+                        all_num[an] = ftell(file)-line-tmp;
+                        an ++;
+                        count_num ++;
+                    }  
+                }
+            }
+            if (feof(file)) keep_reading = false;
+        }
+    }
+    if (all) {
+        for (int a=0;a<an;a++) {
+            printf("%d  " ,all_num[a]);
+        }
+    }
+    else if (at) {
+        if (all_num[num-1]==0) {
+            printf("-1\n");
+        }
+        else printf("%d\n" ,all_num[num-1]);
+    }
+    else if (count) {
+        printf("%d\n" ,count_num);
+    }
+    else {
+        if (all_num[0]==0) {
+            printf("-1\n");
+        }
+        else printf("%d\n" ,all_num[0]);
     }
 
+}
+
+//replace incomplite
+void replace(const char *address,const char *text,const char*text2,char *attribute) {
+    if (strstr(address,"/root/")) strremove(address,"/root/");
+    if (strstr(attribute,"-at")&&strstr(attribute,"all")) {
+        printf("It can't be possible dude!\n");
+        return ;
+    }
+    FILE *file = fopen(address , "r") ;
+    if (file==NULL) {
+        printf("Can't read the file\n");
+        return ;
+    }
+    char buffer[MAX];
+    bool count = false;
+    int count_num = 0;
+    bool at = false ;
+    int num=0;
+    bool byword = false ;
+    bool all = false ;
+    int all_num[MAX];
+    int an = 0;
+    bool wildcard = false;
+    char *temp = (char *)malloc(sizeof(char)*MAX);
+    temp = strtok(attribute," ");
 }
 
 void undo()
@@ -954,6 +1111,7 @@ void undo()
 
 void grep(const char *text,char *addresses,char *attribute)
 {
+    if (strstr(addresses,"/root/")) strremove(addresses,"/root/");
     FILE *file;
     char buffer[MAX];
     char result[MAX][MAX];
@@ -1233,8 +1391,6 @@ void closingpair(char *address) {
     strremove(secondpath," ");
     strcat(secondpath,"temp_");
     strcat(secondpath,nameOFfile) ;
-    //printf("\t%s\n" ,secondpath);
-    //printf("\t%s" ,temp_a);
     temp = fopen(secondpath , "w");
     if (temp==NULL) {
         printf("Error");
@@ -1310,9 +1466,9 @@ void closingpair(char *address) {
             fputc(a,temp);
             pre = a;
         }
-        if (a==EOF) {
-            break;
-        }
+        //if (a==EOF) {
+        //    break;
+        //}
     }   while (a!=EOF);
     fclose(file);
     fclose(temp);
